4c31a428856f190424acc46a333bc05bb88a97fd
Gus <gustave.monce@outlook.com>
2021-02-03 20:05:31 +0100

    Switch to DrvStore + tidy up licensing

---
+++ DriverUpdater/Logging.cs
@@ -0,0 +1,110 @@
+﻿/*
+
+Copyright (c) 2017-2021, The LumiaWOA Authors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+using System;
+
+namespace DriverUpdater
+{
+    internal class Logging
+    {
+        public enum LoggingLevel
+        {
+            Information,
+            Warning,
+            Error
+        }
+
+        private static readonly object lockObj = new object();
+
+        public static void ShowProgress(Int64 CurrentProgress, Int64 TotalProgress, DateTime startTime, bool DisplayRed)
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan timeSoFar = now - startTime;
+
+            TimeSpan remaining = new TimeSpan(0);
+
+            try
+            {
+                remaining = TimeSpan.FromMilliseconds(timeSoFar.TotalMilliseconds / CurrentProgress * (TotalProgress - CurrentProgress));
+            }
+            catch { }
+
+            if (TotalProgress == 0)
+            {
+                TotalProgress = 1;
+                CurrentProgress = 1;
+            }
+
+            Logging.Log(string.Format("{0} {1:hh\\:mm\\:ss\\.f}", GetDismLikeProgBar((Int32)(CurrentProgress * 100 / TotalProgress)), remaining, remaining.TotalHours, remaining.Minutes, remaining.Seconds, remaining.Milliseconds), returnline: false, severity: DisplayRed ? Logging.LoggingLevel.Warning : Logging.LoggingLevel.Information);
+        }
+
+        private static string GetDismLikeProgBar(Int32 perc)
+        {
+            Int32 eqsLength = (Int32)((Double)perc / 100 * 55);
+            string bases = new string('=', eqsLength) + new string(' ', 55 - eqsLength);
+            bases = bases.Insert(28, perc + "%");
+            if (perc == 100)
+                bases = bases.Substring(1);
+            else if (perc < 10)
+                bases = bases.Insert(28, " ");
+            return "[" + bases + "]";
+        }
+
+        public static void Log(string message, LoggingLevel severity = LoggingLevel.Information, bool returnline = true)
+        {
+            lock (lockObj)
+            {
+                if (message == "")
+                {
+                    Console.WriteLine();
+                    return;
+                }
+
+                var msg = "";
+
+                switch (severity)
+                {
+                    case LoggingLevel.Warning:
+                        msg = "  Warning  ";
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case LoggingLevel.Error:
+                        msg = "   Error   ";
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case LoggingLevel.Information:
+                        msg = "Information";
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                }
+
+                if (returnline)
+                    Console.WriteLine(DateTime.Now.ToString("'['HH':'mm':'ss']'") + "[" + msg + "] " + message);
+                else
+                    Console.Write("\r" + DateTime.Now.ToString("'['HH':'mm':'ss']'") + "[" + msg + "] " + message);
+
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
--- DriverUpdater/Program.cs
+++ DriverUpdater/Program.cs
@@ -1,6 +1,31 @@
-﻿using System;
+﻿/*
+
+Copyright (c) 2017-2021, The LumiaWOA Authors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace DriverUpdater
 {
@@ -10,7 +35,7 @@ namespace DriverUpdater
         {
             if (args.Count() < 3)
             {
-                Console.WriteLine("Usage: DriverUpdater <Path to definition> <Path to Driver repository> <Path to Device partition>");
+                Logging.Log("Usage: DriverUpdater <Path to definition> <Path to Driver repository> <Path to Device partition>");
                 return;
             }
 
@@ -20,85 +45,132 @@ namespace DriverUpdater
 
             if (!File.Exists(Definition) || !Directory.Exists(DriverRepo) || !Directory.Exists(DevicePart))
             {
-                Console.WriteLine("Usage: DriverUpdater <Path to definition> <Path to Driver repository> <Path to Device partition>");
+                Logging.Log("Usage: DriverUpdater <Path to definition> <Path to Driver repository> <Path to Device partition>");
                 return;
             }
 
             try
             {
                 Install(Definition, DriverRepo, DevicePart);
-                ResealForPnPFirstBootUx(DevicePart);
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Something happened!");
-                Console.WriteLine(ex.ToString());
+                Logging.Log("Something happened!", Logging.LoggingLevel.Error);
+                Logging.Log(ex.ToString(), Logging.LoggingLevel.Error);
             }
 
-            Console.WriteLine("Done!");
+            Logging.Log("Done!");
         }
 
-        static void ResealForPnPFirstBootUx(string DevicePart)
+        static bool ResealForPnPFirstBootUx(string DevicePart)
         {
-            using var hive = new DiscUtils.Registry.RegistryHive(File.Open(Path.Combine(DevicePart, "Windows\\System32\\config\\SYSTEM"), FileMode.Open, FileAccess.ReadWrite), DiscUtils.Streams.Ownership.Dispose);
-            var hwconf = hive.Root.OpenSubKey("HardwareConfig");
-            if (hwconf != null)
+            try
             {
-                Console.WriteLine("Resealing image to PnP FirstBootUx...");
-                foreach (var subkey in hwconf.GetSubKeyNames())
-                    hwconf.DeleteSubKeyTree(subkey);
-                foreach (var subval in hwconf.GetValueNames())
-                    hwconf.DeleteValue(subval);
+                using var hive = new DiscUtils.Registry.RegistryHive(File.Open(Path.Combine(DevicePart, "Windows\\System32\\config\\SYSTEM"), FileMode.Open, FileAccess.ReadWrite), DiscUtils.Streams.Ownership.Dispose);
+                var hwconf = hive.Root.OpenSubKey("HardwareConfig");
+                if (hwconf != null)
+                {
+                    Logging.Log("Resealing image to PnP FirstBootUx...");
+                    foreach (var subkey in hwconf.GetSubKeyNames())
+                        hwconf.DeleteSubKeyTree(subkey);
+                    foreach (var subval in hwconf.GetValueNames())
+                        hwconf.DeleteValue(subval);
+
+                    return true;
+                }
             }
+            catch { }
+
+            return false;
         }
 
         static void Install(string Definition, string DriverRepo, string DevicePart)
         {
-            Console.WriteLine("Reading definition file...");
-            string[] definitionPaths = File.ReadAllLines(Definition).Where(x => !string.IsNullOrEmpty(x))
-                .Union(new string[] { "components\\ANYSOC\\SUPPORT.DESKTOP.POST_UPGRADE_ENABLEMENT" }).ToArray();
+            Logging.Log("Reading definition file...");
+            string[] definitionPaths = File.ReadAllLines(Definition).Where(x => !string.IsNullOrEmpty(x)).ToArray();
 
-            Microsoft.Dism.DismApi.InitializeEx(Microsoft.Dism.DismLogLevel.LogErrorsWarningsInfo);
-
-            Console.WriteLine("Opening Session...");
-            using var deviceSession = Microsoft.Dism.DismApi.OpenOfflineSession(DevicePart);
-
-            Console.WriteLine("Enumerating existing drivers...");
-            var existingDrivers = Microsoft.Dism.DismApi.GetDrivers(deviceSession, false);
-
-            var orderedExistingDrivers = existingDrivers.OrderBy(driver => $"{driver.ClassDescription}\\{driver.OriginalFileName.Split("\\")[^1]}");
-
-            Console.WriteLine("Uninstalling drivers...");
-
-            foreach (var driver in orderedExistingDrivers)
+            //if (ResealForPnPFirstBootUx(DevicePart))
             {
-                Console.WriteLine($"{driver.ClassDescription}\\{driver.OriginalFileName.Split("\\")[^1]} - Version: {driver.Version} - Date: {driver.Date}");
-                Microsoft.Dism.DismApi.RemoveDriver(deviceSession, driver.PublishedName);
+                definitionPaths = File.ReadAllLines(Definition).Where(x => !string.IsNullOrEmpty(x))
+                    .Union(new string[] { "components\\ANYSOC\\SUPPORT.DESKTOP.POST_UPGRADE_ENABLEMENT" }).ToArray();
             }
 
-            Console.WriteLine("Installing new drivers...");
+            Logging.Log("Enumerating existing drivers...");
+
+            List<string> existingDrivers = new List<string>();
+
+            var ntStatus = NativeMethods.DriverStoreOfflineEnumDriverPackageW(
+                (
+                    string DriverPackageInfPath, 
+                    NativeMethods.DriverStoreOfflineEnumDriverPackageInfoW DriverStoreOfflineEnumDriverPackageInfoW, 
+                    IntPtr Unknown
+                ) =>
+                {
+                    Console.Title = $"Driver Updater - DriverStoreOfflineEnumDriverPackageW - {DriverPackageInfPath}";
+                    if (DriverStoreOfflineEnumDriverPackageInfoW.InboxInf == 0)
+                        existingDrivers.Add(DriverPackageInfPath);
+                    return 1;
+                }
+            , IntPtr.Zero, $"{DevicePart}\\Windows");
+
+            if (ntStatus != 0)
+            {
+                Logging.Log($"DriverStoreOfflineEnumDriverPackageW: ntStatus={ntStatus}", Logging.LoggingLevel.Error);
+                return;
+            }
+
+            Logging.Log("Uninstalling drivers...");
+
+            long Progress = 0;
+            DateTime startTime = DateTime.Now;
+
+            foreach (var driver in existingDrivers)
+            {
+                Console.Title = $"Driver Updater - DriverStoreOfflineDeleteDriverPackageW - {driver}";
+                Logging.ShowProgress(Progress++, existingDrivers.Count, startTime, false);
+                ntStatus = NativeMethods.DriverStoreOfflineDeleteDriverPackageW(driver, 0, IntPtr.Zero, $"{DevicePart}\\Windows", DevicePart);
+
+                if (ntStatus != 0)
+                {
+                    Logging.Log("");
+                    Logging.Log($"DriverStoreOfflineDeleteDriverPackageW: ntStatus={ntStatus}", Logging.LoggingLevel.Error);
+                    return;
+                }
+            }
+            Logging.ShowProgress(existingDrivers.Count, existingDrivers.Count, startTime, false);
+            Logging.Log("");
+
+            Logging.Log("Installing new drivers...");
 
             foreach (var path in definitionPaths)
             {
-                int maxAttempts = 3;
-                int currentAttempts = 0;
-                bool success = false;
-                while (!success)
+                Logging.Log(path);
+
+                // The where LINQ call is because directory can return .inf_ as well...
+                var infs = Directory.EnumerateFiles($"{DriverRepo}\\{path}", "*.inf", SearchOption.AllDirectories)
+                    .Where(x => x.EndsWith(".inf", StringComparison.InvariantCultureIgnoreCase));
+
+                Progress = 0;
+                startTime = DateTime.Now;
+
+                foreach (var inf in infs)
                 {
-                    try
+                    var destinationPath = new StringBuilder(260);
+                    int destinationPathLength = 260;
+
+                    Console.Title = $"Driver Updater - DriverStoreOfflineAddDriverPackageW - {inf}";
+                    Logging.ShowProgress(Progress++, infs.Count(), startTime, false);
+                    ntStatus = NativeMethods.DriverStoreOfflineAddDriverPackageW(inf, 0x00000020 | 0x00000080 | 0x00000100, IntPtr.Zero, 12, "en-US", destinationPath, ref destinationPathLength, $"{DevicePart}\\Windows", DevicePart);
+
+                    if (ntStatus != 0)
                     {
-                        Console.WriteLine(path);
-                        Microsoft.Dism.DismApi.AddDriversEx(deviceSession, $"{DriverRepo}\\{path}", true, true);
-                        success = true;
-                    }
-                    catch
-                    {
-                        if (currentAttempts == maxAttempts)
-                            throw;
-                        Console.WriteLine("Retrying...");
-                        currentAttempts++;
+                        Logging.Log("");
+                        Logging.Log($"DriverStoreOfflineAddDriverPackageW: ntStatus={ntStatus}, destinationPathLength={destinationPathLength}, destinationPath={destinationPath}", Logging.LoggingLevel.Error);
+                        return;
                     }
                 }
+                Logging.ShowProgress(infs.Count(), infs.Count(), startTime, false);
+                Logging.Log("");
             }
         }
     }
