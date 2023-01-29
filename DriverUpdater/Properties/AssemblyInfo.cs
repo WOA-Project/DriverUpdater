67f3c6e223ab2279cb8472f264b3a6fe36baa929
Gustave Monce <gustave.monce@outlook.com>
2023-01-29 17:26:14 +0100

    Fix missing version in tool help

---
+++ DriverUpdater/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Driver Updater")]
+[assembly: AssemblyDescription("Cleans and Installs a new set of drivers onto a Windows Image")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Driver Updater")]
+[assembly: AssemblyCopyright("Copyright © LumiaWoA and DuoWoA authors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.8.0.0")]
+[assembly: AssemblyFileVersion("1.8.0.0")]
