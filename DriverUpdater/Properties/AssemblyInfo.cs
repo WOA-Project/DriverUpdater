02beda9c7688b63dc27424c6f4abb557c81778b3
Gustave Monce <gustave.monce@outlook.com>
2024-11-24 14:50:13 +0100

    v2.1.5.0

--- DriverUpdater/Properties/AssemblyInfo.cs
+++ DriverUpdater/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@
 [assembly: AssemblyCopyright("Copyright © LumiaWoA and DuoWoA authors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.1.4.0")]
-[assembly: AssemblyFileVersion("2.1.4.0")]
+[assembly: AssemblyVersion("2.1.5.0")]
+[assembly: AssemblyFileVersion("2.1.5.0")]
